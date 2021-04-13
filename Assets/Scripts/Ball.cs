37562c08b979c843a18f6536b665424f68dafe2f
Io Adhitama <44990840+IoAdhitama@users.noreply.github.com>
2021-04-13 15:23:22 +0700

    Add function to randomly generate ball at start of match
    Spawning is done through teleporting the ball instead of instantiating it, however.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,21 +4,48 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    GameManager gameManager;
+    [SerializeField] GameManager game;
+
     // Start is called before the first frame update
     void Start()
     {
-        gameManager.OnMatchStart += spawnBall;
+        game.OnMatchStart += HandleOnMatchStart;
+    }
+    
+    void HandleOnMatchStart(object sender, System.EventArgs e)
+    {
+        StartCoroutine(DelayBallSpawning());
     }
 
-    private void spawnBall(object sender, System.EventArgs e)
+    private void SpawnBall()
     {
-        throw new System.NotImplementedException();
+        switch (game.GetGameState())
+        {
+            case GameManager.GameState.BlueAttack:
+                transform.position = new Vector3(Random.Range(10f, -10f), 0.75f, Random.Range(-15f, -5f));
+                break;
+
+            case GameManager.GameState.RedAttack:
+                transform.position = new Vector3(Random.Range(10f, -10f), 0.75f, Random.Range(5f, 15f));
+                break;
+
+            case GameManager.GameState.PenaltyGame:
+                break;
+
+            default:
+                break;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator DelayBallSpawning() // Delay the spawning a bit, till the game state changes
+    {
+        yield return new WaitForSeconds(0.001f);
+        SpawnBall();
     }
 }
